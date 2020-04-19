e16b1ef09ffb9af398195b90ef40cf7360df7720
Evn Sey <evn.seyed@gmail.com>
2020-04-19 22:07:41 +0300

    init

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Feed from '../views/Feed.vue'
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    component: Feed
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/Feed.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    {{$route.path}}
+  </div>
+</template>
+
