1732072f9ce86a0903f4e7a34a7f78a53f77a550
Evn Sey <evn.seyed@gmail.com>
2020-04-23 12:12:46 +0300

    Create simple registration, signin form

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,51 @@
 <template>
   <div id="app">
+    <div v-if="currentUserName">
+      {{ 'Hello, ' + currentUserName +'!'}}
+    </div>
+
+    <input v-model="email" placeholder="email">
+    <input v-model="password" placeholder="password">
+    
+    <button @click="$store.dispatch('registerUser')">
+      Register
+    </button>
+    <button @click="$store.dispatch('logIn')">
+      Sign In
+    </button>
+    <button @click="$store.dispatch('logOut')">
+      Sign Out
+    </button>
+
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  created() {
+    this.$store.dispatch('subscribeToUserAuth')
+  },
+  computed: {
+    email: {
+      set(newValue) {
+        this.$store.commit('setLoginEmail', newValue)
+      },
+      get() {
+        return this.$store.state.loginData.email
+      }
+    },  
+    password: {
+      set(newValue) {
+        this.$store.commit('setLoginPassword', newValue)
+      },
+      get() {
+        return this.$store.state.loginData.password
+      }
+    },
+    currentUserName() {
+      return this.$store.state.authUser?.email
+    }
+  }
+}
+</script>
